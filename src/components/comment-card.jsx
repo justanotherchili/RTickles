19f3c201d2ea9fa238ab5da5800fe50e624c214d
justanotherchili <chiyan.li@hotmail.com>
2024-07-09 18:00:10 +0100

    fixed styling for comment cards

--- src/components/comment-card.jsx
+++ src/components/comment-card.jsx
@@ -2,11 +2,13 @@ import "../styles/comment-card.css";
 import UserContext from "../contexts/User";
 import { useContext } from "react";
 import { deleteCommentByID } from "../api";
+import { formatDistanceToNow } from "date-fns/formatDistanceToNow";
 
 function CommentCard(props) {
   const { comment, commentsArray, setCommentsArray } = props;
   const { currUser } = useContext(UserContext);
   const tempCommentsArray = [...commentsArray];
+
   async function handleDelete() {
     try {
       const arrayAfterDelete = commentsArray.filter(
@@ -22,17 +24,22 @@ function CommentCard(props) {
   }
 
   return (
-    <>
-      <section className="comment-card">
-        <p>{comment.author}</p>
-        <p>{comment.body}</p>
-        <p>votes: {comment.votes}</p>
-        <p>Posted: {new Date(comment.created_at).toLocaleString()}</p>
-        {comment.author === currUser.username && (
-          <button onClick={handleDelete}>X</button>
-        )}
-      </section>
-    </>
+    <section className="comment-card">
+      <div className="comment-card-header">
+        <p className="author">{comment.author}</p>
+        <p className="posted">
+          {" "}
+          {formatDistanceToNow(new Date(comment.created_at), {
+            addSuffix: true,
+          })}
+        </p>
+      </div>
+      <p className="body">{comment.body}</p>
+      <p className="votes">{comment.votes} Votes</p>
+      {comment.author === currUser.username && (
+        <button onClick={handleDelete}>Delete</button>
+      )}
+    </section>
   );
 }
 
